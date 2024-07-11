d98a91a068484cb5125de9b1c3f483c37f1ab5c5
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 20:21:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", (e) => {
+    let p = document.querySelector('p')
+    p.textContent = "This is really cool!";
+  }
+     
+  );
